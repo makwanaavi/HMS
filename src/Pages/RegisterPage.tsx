e6a39b433068aae4a2ee829c995e4044b83f9015
prabhu-woyce <pparmar@woyce.io>
2025-07-17 12:20:09 +0530

    #update RegisterPage ui

--- src/Pages/RegisterPage.tsx
+++ src/Pages/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import { Button, PasswordInput, TextInput } from '@mantine/core';
+import { Button, PasswordInput, SegmentedControl, TextInput } from '@mantine/core';
 import { IconHeartbeat } from '@tabler/icons-react';
 import { useForm } from '@mantine/form';
 import { Link } from 'react-router-dom';
@@ -6,13 +6,17 @@ import { Link } from 'react-router-dom';
 const RegisterPage = () => {
   const form = useForm({
     initialValues: {
+      type: 'PATIENT',
       email: '',
       password: '',
+      confirmPassword: '',
     },
 
     validate: {
       email: (value: any) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
       password: (value: any) => (!value ? 'Password Required' : null),
+      confirmPassword: (value: any, values: any) =>
+        value === values.password ? null : "Password don't match",
     },
   });
   const handleSubmit = (values: typeof form.values) => {
@@ -34,16 +38,23 @@ const RegisterPage = () => {
           onSubmit={form.onSubmit(handleSubmit)}
           className="flex flex-col gap-5 [&_input]:placeholder-neutral-100 [&_input]:!pl-2 [&_input]:!text-white [&_svg]:text-white [&_.mantine-Input-input]:border-white focus-within:[&_.mantine-Input-input]:!border-pink-500 [&_.mantine-Input-input]:!border"
         >
-          <div className="self-center font-heading font-medium text-light text-2xl">Login</div>
-          <TextInput
-            {...form.getInputProps('email')}
-            className="transition duration-300"
-            variant="unstyled"
+          <div className="self-center font-heading font-medium text-light text-2xl">Register</div>
+          <SegmentedControl
+            {...form.getInputProps('type')}
+            fullWidth
             size="md"
             radius="md"
-            placeholder="Email"
+            color="pink"
+            bg="none"
+            className="[&_*]:!text-white border border-white"
+            data={[
+              { label: 'Patient', value: 'PATIENT' },
+              { label: 'Doctor', value: 'DOCTOR' },
+              { label: 'Admin', value: 'ADMIN' },
+            ]}
           />
-           <TextInput
+          ;
+          <TextInput
             {...form.getInputProps('email')}
             className="transition duration-300"
             variant="unstyled"
@@ -59,12 +70,22 @@ const RegisterPage = () => {
             radius="md"
             placeholder="Password"
           />
+          <PasswordInput
+            {...form.getInputProps('confirmPassword')}
+            className="transition duration-300"
+            variant="unstyled"
+            size="md"
+            radius="md"
+            placeholder="confirm Password"
+          />
           <Button type="submit" color="pink" variant="filled" radius="md" size="md">
             Login
           </Button>
-          
-          <div className='text-neutral-100 text-sm self-center'>
-         haven an account? <Link to='/login' className='hover:underline text-sm text-pink-400 font-semibold'>Login</Link>
+          <div className="text-neutral-100 text-sm self-center">
+            haven an account?{' '}
+            <Link to="/login" className="hover:underline text-sm text-pink-400 font-semibold">
+              Login
+            </Link>
           </div>
         </form>
       </div>
